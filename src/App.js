ef862dc11d0325a473b84abfe04bb249d7cfc797
Andrey <andrey.kr.love@gmail.com>
2025-01-27 21:28:24 +0800

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { WagmiConfig, createClient, configureChains } from 'wagmi';
+import { publicProvider } from 'wagmi/providers/public';
+import { RainbowKitProvider, ConnectButton } from '@rainbow-me/rainbowkit';
+import { mainnet, polygon, optimism, arbitrum } from 'wagmi/chains';
+import '@rainbow-me/rainbowkit/styles.css';
+
+// Configure chains and client
+const { chains, provider } = configureChains(
+  [mainnet, polygon, optimism, arbitrum],
+  [publicProvider()]
+);
+const wagmiClient = createClient({
+  autoConnect: true,
+  connectors: () => [],
+  provider,
+});
+
+function App() {
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider chains={chains}>
+        <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh' }}>
+          <ConnectButton />
+        </div>
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
+}
+
+export default App;
