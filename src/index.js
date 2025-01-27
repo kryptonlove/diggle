e274dcaabe2126a75fdca1b5f8539b748df17036
Andrey <andrey.kr.love@gmail.com>
2025-01-27 21:28:46 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,33 +1,6 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import { WagmiConfig, createClient, configureChains } from "wagmi";
-import { publicProvider } from "wagmi/providers/public";
-import { RainbowKitProvider, ConnectButton } from "@rainbow-me/rainbowkit";
-import { mainnet, polygon, optimism, arbitrum } from "wagmi/chains";
-import "@rainbow-me/rainbowkit/styles.css";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
 
-// Configure chains and client
-const { chains, provider } = configureChains(
-  [mainnet, polygon, optimism, arbitrum],
-  [publicProvider()]
-);
-const wagmiClient = createClient({
-  autoConnect: true,
-  connectors: () => [],
-  provider,
-});
-
-function App() {
-  return (
-    <WagmiConfig client={wagmiClient}>
-      <RainbowKitProvider chains={chains}>
-        <div style={{ display: "flex", justifyContent: "center", alignItems: "center", height: "100vh" }}>
-          <ConnectButton />
-        </div>
-      </RainbowKitProvider>
-    </WagmiConfig>
-  );
-}
-
-const root = document.getElementById("root");
+const root = document.getElementById('root');
 ReactDOM.render(<App />, root);
